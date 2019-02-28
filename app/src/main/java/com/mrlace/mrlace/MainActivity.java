ff218c87ebfcd6235441424f83b5b5653bdf8796
tonnymuchui <tonykanyingah@gmail.com>
2019-02-28 19:24:40 +0300

    initialize login& sign up button

--- app/src/main/java/com/mrlace/mrlace/MainActivity.java
+++ app/src/main/java/com/mrlace/mrlace/MainActivity.java
@@ -2,12 +2,19 @@ package com.mrlace.mrlace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+@BindView(R.id.main_ligin_btn) Button loginButton;
+    @BindView(R.id.join_now_btn) Button Sign_upButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
