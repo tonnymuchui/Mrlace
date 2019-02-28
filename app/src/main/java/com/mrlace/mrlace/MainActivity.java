7661a28e9712722d825560481fd7dc48aacf5be1
tonnymuchui <tonykanyingah@gmail.com>
2019-02-28 23:32:58 +0300

    added UI in login activity

--- app/src/main/java/com/mrlace/mrlace/MainActivity.java
+++ app/src/main/java/com/mrlace/mrlace/MainActivity.java
@@ -1,20 +1,31 @@
 package com.mrlace.mrlace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.mrlace.mrlace.Auth.LoginActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-@BindView(R.id.main_ligin_btn) Button loginButton;
-    @BindView(R.id.join_now_btn) Button Sign_upButton;
+@BindView(R.id.main_ligin_btn) Button main_loginButton;
+    @BindView(R.id.main_join_now_btn) Button main_Sign_upButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        main_loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
