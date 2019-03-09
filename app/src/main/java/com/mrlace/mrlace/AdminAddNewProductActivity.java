fd766f332f27dc5c53bca96c837977dd2c9fef88
tonnymuchui <tonykanyingah@gmail.com>
2019-03-10 02:36:46 +0300

    fixing bug on adminAddNewProduct activity

--- app/src/main/java/com/mrlace/mrlace/AdminAddNewProductActivity.java
+++ app/src/main/java/com/mrlace/mrlace/AdminAddNewProductActivity.java
@@ -3,10 +3,9 @@ package com.mrlace.mrlace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.rey.material.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,10 +13,11 @@ import butterknife.ButterKnife;
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
-    @BindView(R.id.select_product_image) ImageView select_product_image;
-    @BindView(R.id.product_name) EditText product_name;
-    @BindView(R.id.product_description) EditText product_description;
-    @BindView(R.id.product_price) EditText product_price;
+
+    @BindView(R.id.admin_product_name) EditText product_name;
+    @BindView(R.id.admin_product_description) EditText product_description;
+    @BindView(R.id.admin_product_price) EditText product_price;
+    @BindView(R.id.admin_select_product_image) ImageView select_product_image;
     @BindView(R.id.add_new_product) Button add_new_product;
 
     @Override
