21f5d68c5e97800bf046e8483127e9074b7c7cbd
tonnymuchui <tonykanyingah@gmail.com>
2019-03-10 02:17:13 +0300

    initializing AddNewProduct layout fields

--- app/src/main/java/com/mrlace/mrlace/AdminAddNewProductActivity.java
+++ app/src/main/java/com/mrlace/mrlace/AdminAddNewProductActivity.java
@@ -2,15 +2,29 @@ package com.mrlace.mrlace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.rey.material.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
+    @BindView(R.id.select_product_image) ImageView select_product_image;
+    @BindView(R.id.product_name) EditText product_name;
+    @BindView(R.id.product_description) EditText product_description;
+    @BindView(R.id.product_price) EditText product_price;
+    @BindView(R.id.add_new_product) Button add_new_product;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
+        ButterKnife.bind(this);
 
         categoryName = getIntent().getExtras().get("category").toString();
 
