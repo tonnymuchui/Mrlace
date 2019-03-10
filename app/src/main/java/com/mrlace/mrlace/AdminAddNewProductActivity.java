a8c399e73a2e29041b888844ecf078ae5c930fc7
tonnymuchui <tonykanyingah@gmail.com>
2019-03-10 13:15:59 +0300

    opening gallery to select image in adminAddNewProductActivity

--- app/src/main/java/com/mrlace/mrlace/AdminAddNewProductActivity.java
+++ app/src/main/java/com/mrlace/mrlace/AdminAddNewProductActivity.java
@@ -1,7 +1,11 @@
 package com.mrlace.mrlace;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,6 +23,8 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
     @BindView(R.id.admin_product_price) EditText product_price;
     @BindView(R.id.admin_select_product_image) ImageView select_product_image;
     @BindView(R.id.add_new_product) Button add_new_product;
+    private static final int GalleryPick = 1;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,33 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         categoryName = getIntent().getExtras().get("category").toString();
 
         Toast.makeText(this, categoryName, Toast.LENGTH_SHORT).show();
+
+        select_product_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+    }
+
+    private void openGallery() {
+
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GalleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GalleryPick && resultCode == RESULT_OK && data!=null){
+            imageUri = data.getData();
+            select_product_image.setImageURI(imageUri);
+
+        }
 
     }
 }
