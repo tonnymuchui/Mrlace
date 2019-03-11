ca6e6a4fa47aaf28b559b38766e40b74cfae443d
tonnymuchui <tonykanyingah@gmail.com>
2019-03-11 14:59:04 +0300

    created new Home Activity with Navigation drower

--- app/src/main/java/com/mrlace/mrlace/Auth/LoginActivity.java
+++ app/src/main/java/com/mrlace/mrlace/Auth/LoginActivity.java
@@ -17,7 +17,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.mrlace.mrlace.AdminAddNewProductActivity;
 import com.mrlace.mrlace.AdminCategoryActivity;
 import com.mrlace.mrlace.HomeActivity;
 import com.mrlace.mrlace.Model.Users;
